a3708a216e3bd57bd83e0f6217e610bf8df333eb
sntkn <mail.tsurumi@gmail.com>
2022-09-10 13:49:08 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+brew install git starship lsd bat jq peco	ghq tig tmux tree git-delta docker vscode
+brew install --cask fig iterm2 obsidian font-hack-nerd-font
+
+cp .vimrc ~/
+cp .zshrc ~/
+source ~/.zshrc
